08432fade70365b762f5f1af1c3c6779d30e8c57
rezaabaskhanian <rezaabaskhanian1367kash@gmail.com>
2026-04-25 09:29:32 +0330

    add new profile

---
+++ internal/delivery/httpserver/user/profile.go
@@ -0,0 +1,40 @@
+package userhandler
+
+import (
+	"aramina/internal/config"
+	"aramina/internal/pkg/richerror"
+	authservice "aramina/internal/service/auth"
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func getClaims(c echo.Context) (*authservice.Claims, error) {
+
+	claims, ok := c.Get(config.AuthMiddlewareContextKey).(*authservice.Claims)
+	if !ok || claims == nil {
+		return nil, fmt.Errorf("claims not found in context")
+	}
+
+	return c.Get(config.AuthMiddlewareContextKey).(*authservice.Claims), nil
+
+}
+
+func (h Handler) Profile(c echo.Context) error {
+
+	const op = "userhandler.Profile"
+
+	claims, err := getClaims(c)
+	if err != nil {
+		return richerror.New(op).WithErr(err)
+	}
+
+	res, err := h.userSvc.Profile(claims.UserID)
+
+	if err != nil {
+		return richerror.New(op).WithErr(err)
+	}
+
+	return c.JSON(http.StatusOK, res)
+}
